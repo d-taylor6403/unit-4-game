17f8ffc65be6e5d31e8db397380b37c3f0294c52
dannitay <dannielletaylor@outlook.com>
2020-01-12 19:45:45 -0600

    intial commit of game logic, updated css and html to accomodate logic

---
+++ assets/javascript/game.js
@@ -0,0 +1,74 @@
+//global variables
+    
+    var winsCounter = 0;
+    var lossesCounter = 0;
+    var userScore = 0;
+    var generatedNumber = 0;
+    var crystalNumber = [];
+    
+
+
+//on page load
+$(document).ready(function(){
+
+$('#wins').text(winsCounter);
+$('#losses').text(lossesCounter);
+
+//Creates random number when page loads
+function getNumber() {
+
+    var minNumber = 19;
+    var maxNumber = 120;
+    generatedNumber = Math.floor(Math.random () * (maxNumber + 1) + minNumber);
+    $('#generatedNumber').html(generatedNumber);
+}
+getNumber();
+
+//Generate random number 1-12 and push into array
+for (var i = 0; i<4; i++){
+    crystalNumber.push(Math.floor(Math.random() * 12)+1);
+    }
+    
+    console.log(crystalNumber);
+
+
+//Reset Game
+function reset (){
+    getNumber ();
+    for (var i = 0; i<4; i++){
+        crystalNumber.push(Math.floor(Math.random() * 12)+1);
+        }
+        
+        console.log(crystalNumber);
+    userScore=0;
+    $('#userScore').text(userScore);
+}
+
+//display on Wins
+function userWin (){
+    winsCounter++;
+    $('#wins').text(winsCounter);
+    reset();
+}
+
+//display on Losses
+function userLose (){
+    lossesCounter++;
+    $('#losses').text(lossesCounter);
+    reset();
+}
+
+//Generate click events for each crystal button
+$('#crystal1').on('click', function (){
+    userScore = userScore + crystalNumber[0];
+    console.log("New Userscore= " + userScore);
+    $('#userScore').text(userScore);
+        //Evaluates win/lose condition
+        if(userScore === generatedNumber){
+            userWin();
+        }else if (userScore > generatedNumber){
+            userLose();
+        }
+})
+
+})
